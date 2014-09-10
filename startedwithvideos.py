60742f4310848683abe0b0fba4477faf30acf08e
Generacy <generacy@gmail.com>
2014-09-09 21:12:35 -0400

    Init commit

---
+++ startedwithvideos.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('face2.avi')
+print cap
+print cap.grab()
+while(True):
+
+    print cap.isOpened()
+    cap.open('face2.avi')
+    print cap.isOpened()
+    print cap.get(3)
+    print cap.grab()
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
